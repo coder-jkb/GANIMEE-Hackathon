a8227a04b625da8e23dd052e848c42ecfac90f91
coder-jkb <jkb17codes@gmail.com>
2022-08-10 15:43:31 +0530

    Created course.html 4 sample videos

--- django_mysite/finance/views.py
+++ django_mysite/finance/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-   return ( render(request,'finance/index.html') )
+   return render(request,'finance/index.html')
     # return HttpResponse("Hello, world. You're at the polls index.")
+
+def course(request):
+   return render(request, 'finance/course.html')
